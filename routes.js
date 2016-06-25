8493e1205bd08b1689440aec0c7b5ce2b52407cb
Cesar Salazar <cesarsalazar09@gmail.com>
2016-06-25 15:54:52 -0500

    Deleted homepage.js, created routes.js to handle all routing, added gitignore, and worked on templates and their styling

--- routes.js
+++ routes.js
@@ -6,7 +6,16 @@ router.get("/", function(request, response){
 
 	response.render("../views/homepage", {us: 'We are boss ass bitches!'});
 
+	console.log(request.url);
+
 });
+
+router.get("/origami-library", function(request, response){
+
+
+	response.render("../views/origami-library", {text: "We Badasss!"});
+	console.log(request.url);
+})
 
 
 module.exports = router; 
--- server.js
+++ server.js
@@ -2,11 +2,16 @@ var express = require("express");
 
 var app = express();
 
-var homepage = require('./routes/homepage');
+var path = require("path");
+
+var routes = require('./routes');
 
 app.set('view engine', 'ejs');
 
-app.use('/', homepage);
+app.use(express.static(path.join(__dirname,"/public")));
+
+app.use('/', routes);
+
 
 app.listen(3000, function(){
 
