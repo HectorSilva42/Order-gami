89b3578dc650d4b4c9be1056353221f9bfe59a71
Cesar Salazar <cesarsalazar09@gmail.com>
2016-06-25 16:14:17 -0500

    resolve conflicts completely

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require("express");
 
 var app = express();
 
-<<<<<<< HEAD
 var path = require("path");
 
 var routes = require('./routes');
@@ -12,15 +11,6 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname,"/public")));
 
 app.use('/', routes);
-=======
-var homepage = require('./routes/homepage');
-var origami_library = require('./routes/origami_library.js');
-
-app.set('view engine', 'ejs');
-
-app.use('/', homepage);
-app.use('/origami_library', origami_library);
->>>>>>> bfe8ca35bc24eaa328c23bd90a63987569796d34
 
 
 app.listen(3000, function(){
